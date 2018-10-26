874d036b1227b19e30e5bc892ea673480dbc274c
Harry Bloch <harryfbloch@gmail.com>
2018-10-26 23:57:52 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,7 +67,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  movieArray = Movie.all.find {|m| m.release_date > 2002 }
+  movieArray = Movie.all.collect {|m| m.release_date > 2002 }
   binding.pry
 end
 
